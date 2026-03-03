178a885f42755650cbc382edbd230ff26c05d58d
DebHatim <thos.debboun.hatim@gmail.com>
2026-03-04 00:10:52 +0100

    Arreglo en imports

--- src/main/java/com/debboun/reservas/controladores/LoginController.java
+++ src/main/java/com/debboun/reservas/controladores/LoginController.java
@@ -2,7 +2,6 @@ package com.debboun.reservas.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller 			 // Marcar como controller para que Spring lo detecte
 public class LoginController {
