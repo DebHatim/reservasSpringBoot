ed078c84247f7d0732fedf9ba008a74e69568154
DebHatim <thos.debboun.hatim@gmail.com>
2026-03-03 01:21:54 +0100

    Añadido un metodo al controlador indice para poder mostrar una lista de hoteles

--- src/main/java/com/debboun/reservas/controladores/IndexController.java
+++ src/main/java/com/debboun/reservas/controladores/IndexController.java
@@ -1,8 +1,25 @@
 package com.debboun.reservas.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import com.debboun.reservas.servicios.implementaciones.AnonimoServiceImplementacion;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor // Crear un constructor para todos los final
+@Controller 			 // Marcar como controller para que Spring lo detecte
+@RequestMapping("/")	 // Ruta raiz
 public class IndexController {
+	
+	private final AnonimoServiceImplementacion anonimoService;
+	
+	@GetMapping
+	public String index(Model modelo) {
+		modelo.addAttribute("hoteles", anonimoService.consultarHoteles());
+		return "index";
+	}
 
 }
