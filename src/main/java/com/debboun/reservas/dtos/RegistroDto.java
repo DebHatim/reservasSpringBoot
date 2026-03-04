01515a5275fc8325bc25c44699c31f4c7b60f4ae
DebHatim <thos.debboun.hatim@gmail.com>
2026-03-04 01:30:03 +0100

    Cambios en el DTO para mostrar mensajes de error

--- src/main/java/com/debboun/reservas/dtos/RegistroDto.java
+++ src/main/java/com/debboun/reservas/dtos/RegistroDto.java
@@ -4,7 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record RegistroDto(@NotBlank String nombre, @NotBlank String apellido, @NotBlank String telefono,
-		@Email @NotBlank String email, @Size(min = 8) String password) {
+// Mensajes en cada dato para mostrar si hay errores, confirmPassword para comprobar que las contraseñas coincidan 
+public record RegistroDto (
+		@NotBlank @Size(min=3, message = "El nombre debe contener al menos 3 caracteres") String nombre,
+		@NotBlank @Size(min=3, message = "El apellido debe contener al menos 3 caracteres") String apellido, 
+		@NotBlank @Size(min=9, message = "El teléfono debe contener al menos 9 dígitos") String telefono,
+		@Email(message = "Formato de email inválido") String email, 
+		@Size(min = 8, message = "La contraseña debe contener al menos 8 caracteres") String password,
+		@Size(min = 8, message = "La contraseña debe contener al menos 8 caracteres") String confirmPassword ) {
 
 }
