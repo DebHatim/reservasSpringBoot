2d9adc61481d9c776c8d94dbbb8bd3e5e29afcd1
DebHatim <thos.debboun.hatim@gmail.com>
2026-03-12 19:04:29 +0100

    Dtos creados para poder mover información correctamente

---
+++ src/main/java/com/debboun/reservas/dtos/EditarUsuarioDto.java
@@ -0,0 +1,5 @@
+package com.debboun.reservas.dtos;
+
+public record EditarUsuarioDto(Long id, String nombre, String apellido, String telefono, String email) {
+
+}
---
+++ src/main/java/com/debboun/reservas/dtos/UsuarioListaDto.java
@@ -0,0 +1,4 @@
+package com.debboun.reservas.dtos;
+
+public record UsuarioListaDto(Long id, String nombre, String email, String rol) {
+}
