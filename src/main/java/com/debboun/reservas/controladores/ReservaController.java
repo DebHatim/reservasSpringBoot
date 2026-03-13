243f4d7b17dfb60f5881c976efa17fc7bc80afc0
DebHatim <thos.debboun.hatim@gmail.com>
2026-03-13 20:25:39 +0100

    Cambiado el controlador para recibir y separar los datos al eliminar una reserva

--- src/main/java/com/debboun/reservas/controladores/ReservaController.java
+++ src/main/java/com/debboun/reservas/controladores/ReservaController.java
@@ -5,7 +5,9 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.debboun.reservas.dtos.ReservaDto;
 import com.debboun.reservas.servicios.UsuarioService;
@@ -15,13 +17,20 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor // Crear un constructor para todos los final
 @Controller
+@RequestMapping("/reserva")
 public class ReservaController {
 	
 	private final UsuarioService usuarioService;
 
-	@PostMapping("/reserva")
+	@PostMapping
 	public String reservar(@Valid @ModelAttribute("ReservaDto") ReservaDto reservaDto, Principal principal, Model modelo) {
 		usuarioService.reservar(reservaDto, principal.getName());
 		return "redirect:/perfil"; // redirigir para limpiar el formulario
+	}
+	
+	@PostMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable Long id) {
+		usuarioService.eliminarReserva(id);
+		return "redirect:/perfil"; // actualizar
 	}
 }
