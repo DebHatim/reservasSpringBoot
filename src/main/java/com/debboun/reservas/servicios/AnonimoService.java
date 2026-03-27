dbe9223b881576023f8368ce5172cea4970acbdf
DebHatim <thos.debboun.hatim@gmail.com>
2026-03-27 18:13:58 +0100

    Eliminado anonimoservice por mala practica

--- src/main/java/com/debboun/reservas/servicios/AnonimoService.java
+++
@@ -1,9 +0,0 @@
-package com.debboun.reservas.servicios;
-
-import com.debboun.reservas.dtos.RegistroDto;
-import com.debboun.reservas.entidades.Hotel;
-
-public interface AnonimoService {
-	Iterable<Hotel> consultarHoteles(); // Usuario anonimo ve los hoteles
-	void registrarUsuario(RegistroDto registroDto); // Usuario anonimo se registra
-}
