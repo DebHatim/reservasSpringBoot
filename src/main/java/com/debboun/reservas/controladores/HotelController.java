bc6bf61cb26864c6aa714d6901a5ae4a38065b1c
DebHatim <thos.debboun.hatim@gmail.com>
2026-08-03 15:05:10 +0200

    fix: evitar error al abrir hoteles inexistentes

--- src/main/java/com/debboun/reservas/controladores/HotelController.java
+++ src/main/java/com/debboun/reservas/controladores/HotelController.java
@@ -13,16 +13,19 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @Controller // Clase para controlar las acciones en la vista de hoteles
 public class HotelController {
-	
+
 	private final HotelService hotelService;
 
 	@GetMapping("/hotel/{id}")
 	public String verHotel(@PathVariable Long id, Model modelo) {
-		MostrarHotelDto hotel = hotelService.buscarPorId(id);
-		if (hotel == null) {
+		MostrarHotelDto hotel;
+		try {
+			hotel = hotelService.buscarPorId(id);
+		} catch (RuntimeException e) {
 			return "redirect:/";
 		}
 		modelo.addAttribute("hotel", hotel);
 		return "hotel";
 	}
 }
+
